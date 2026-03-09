34a42f15307e04b2d20a3bff5c4b06129ae3ef4d
Elie <web@elieageron.com>
2026-03-09 17:53:56 +0100

    chore: add entry comment to main.tsx for better code organization

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot, hydrateRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App.tsx";
 import "./index.css";
+// entry
 
 const rootEl = document.getElementById("root")!;
 const app = (
